ab70f78a1ba42f5f26d3dce95d06d6138678ae01
RoeiDayan <roeidayan2007@gmail.com>
2025-03-26 13:25:44 +0200

    Added creation method for membercommunity

--- CommunityAppServer/DTO/MemberCommunity.cs
+++ CommunityAppServer/DTO/MemberCommunity.cs
@@ -2,6 +2,7 @@
 {
     public class MemberCommunity
     {
+        public MemberCommunity() { }
         public Member Member { get; set; }  // Membership data
         public Community Community { get; set; }  // Community data
     }
